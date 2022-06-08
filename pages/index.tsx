1714b747e661941f1d62d739d23281faa3790c67
gkssk1026 <gkssk1026@hanmail.net>
2022-06-08 20:58:06 +0900

    tsx 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <h1>Trip</h1>
-      </Head>
+      <h1>Trip</h1>
 
       <main className={styles.main}>
         <h2>main</h2>
