50285e5c8a273ac3dd1a989f2ee16f3f4dfd3812
gkssk1026 <gkssk1026@hanmail.net>
2022-07-06 16:38:01 +0900

    슬라이드일부수정

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -11,28 +11,32 @@ export default class SimpleSlider extends Component {
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      centerMode: true,
+      centerPadding: "0px",
     };
     return (
       <div>
-        <h2> Single Item</h2>
+        <h2></h2>
         <Slider {...settings}>
           <div>
             <h3>제주도항공권</h3>
           </div>
           <div>
-            <h3>2</h3>
+            <h3>신라호텔</h3>
           </div>
           <div>
-            <h3>3</h3>
+            <h3>돌아온 동남아 특가</h3>
           </div>
           <div>
-            <h3>4</h3>
+            <h3>초특가 물놀이</h3>
           </div>
           <div>
-            <h3>5</h3>
+            <h3>5성급 수영장</h3>
           </div>
           <div>
-            <h3>6</h3>
+            <h3>여행자보험</h3>
           </div>
         </Slider>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,17 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "../components/Slider";
+import Cardmedia from "../components/CardMedia";
 
 export default function MediaCard() {
   return (
     <>
-      <>
-        <div></div>
-        <Card sx={{ maxWidth: 345 }}>
-          <Typography gutterBottom variant="h5" component="div">
-            제주도
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            3,150여 개의 여행상품
-          </Typography>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/img/jeju.jpg"
-            alt="jeju"
-          />
-          <CardContent></CardContent>
-          <CardActions>
-            <Button size="small">둘러보기</Button>
-          </CardActions>
-        </Card>
-      </>
+      <div>
+        <h1>여행이 돌아왔어요.</h1>
+      </div>
+
+      <Cardmedia></Cardmedia>
       <Slider></Slider>
     </>
   );
