0ca1c186cf19800ca3be2bbe043f7d75fb046326
gkssk1026 <gkssk1026@hanmail.net>
2022-07-12 22:07:58 +0900

    데이터map적용

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,45 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import Slider2 from "react-slick";
+import sliderTypeProps from "../ts/interface/slider/slider";
 
-export default class SimpleSlider extends Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      centerMode: true,
-      centerPadding: "0px",
-    };
-    return (
-      <div>
-        <h2></h2>
-        <Slider {...settings}>
-          <div>
-            <h3>제주도항공권</h3>
-          </div>
-          <div>
-            <h3>신라호텔</h3>
-          </div>
-          <div>
-            <h3>돌아온 동남아 특가</h3>
-          </div>
-          <div>
-            <h3>초특가 물놀이</h3>
-          </div>
-          <div>
-            <h3>5성급 수영장</h3>
-          </div>
-          <div>
-            <h3>여행자보험</h3>
-          </div>
-        </Slider>
-      </div>
-    );
-  }
+interface sliderProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sliderProps: any;
+  sliderData: sliderTypeProps[];
 }
+
+const Slider: React.FC<sliderProps> = (props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    centerPadding: "0px",
+  };
+
+  return (
+    <div>
+      <h2></h2>
+      <Slider2 {...settings}>
+        {props.sliderData.map((item) => (
+          <li key={item.id}>
+            {item.title}
+            {item.image}
+          </li>
+        ))}
+      </Slider2>
+    </div>
+  );
+};
+
+export default Slider;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,18 @@ import * as React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "../components/Slider";
-import Cardmedia from "../components/CardMedia";
+import Cardmedia from "../components/Cardmedia";
+import sliderTypeProps from "../ts/interface/slider/slider";
 
 export default function MediaCard() {
+  const sliderData: sliderTypeProps[] = [
+    { id: 1, title: "제주항공권", image: "aa" },
+    { id: 2, title: "신라호텔", image: "a" },
+    { id: 3, title: "돌아온 동남아 특가", image: "bb" },
+    { id: 4, title: "초특가 물놀이", image: "b" },
+    { id: 5, title: "5성급 수영장", image: "c" },
+    { id: 6, title: "여행자보험", image: "cc" },
+  ];
   return (
     <>
       <div>
@@ -12,7 +21,7 @@ export default function MediaCard() {
       </div>
 
       <Cardmedia></Cardmedia>
-      <Slider></Slider>
+      <Slider sliderData={sliderData} sliderProps={undefined}></Slider>
     </>
   );
 }
